0c25458fd624e920bb2552bccc19ff8d12c9a6d6
Htrix <htrix15@gmail.com>
2020-08-30 13:10:10 +0300

    Move connect section to new component

---
+++ ChatProject/ChatProject/ClientApp/src/app/components/connect/connect.component.ts
@@ -0,0 +1,105 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { DataShell } from '../../models/data-shell'
+import { HttpErrorResponse } from '@angular/common/http';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ChatGroup } from '../../models/chat-group';
+import { Router } from '@angular/router';
+import { ChatingService } from '../../services/chating.service';
+import { TypeChecker} from '../../services-classes/type-checker'
+
+@Component({
+  selector: 'app-connect',
+  templateUrl: './connect.component.html',
+  styleUrls: ['./connect.component.scss']
+})
+export class ConnectComponent implements OnInit {
+
+  public inputChatNameForm: FormGroup;
+  public inputNickForm: FormGroup;
+  public inputPasswordForm: FormGroup;
+
+  public chatName: string;
+  public nick: string;
+  public privateChat: boolean;
+
+  public chatGroup: ChatGroup;
+
+  constructor(
+    private dataService:DataService, 
+    private router: Router,
+    private chatingService: ChatingService){
+      this.inputChatNameForm = new FormGroup({
+        chatName: new FormControl(null)
+    });
+
+    this.inputPasswordForm = new FormGroup({
+      password: new FormControl(null)
+    });
+
+    this.inputNickForm = new FormGroup({
+      nick: new FormControl(null)
+    });
+
+    this.privateChat = false;
+  }
+
+  ngOnInit(): void {
+  }
+
+  connectToChat(): void{
+
+    let chatName = this.inputChatNameForm.controls['chatName']?.value;
+    this.nick = this.inputNickForm.controls['nick']?.value;
+    if(chatName && TypeChecker.checkType<string>(chatName, 'length') && this.nick && TypeChecker.checkType<string>(this.nick, 'length')){
+      this.dataService
+      .getUserDatas('check-group-and-nick', new Map<string, string>().set('groupName', chatName).set('nick', this.nick))
+      .subscribe(
+        (chatGroup: DataShell) => {
+        if(chatGroup.data && TypeChecker.checkType<ChatGroup>(chatGroup.data, 'Private')){
+            this.chatGroup = chatGroup.data;
+            if(this.chatGroup.Private){
+              this.privateChat = true;
+            } else{
+              this.chatingService
+              .connectToChat(this.chatGroup.Name, this.nick)
+              .subscribe(
+                () => {
+                  console.log('connection success');
+                  this.router.navigate(['/chat',this.chatGroup.Id])},
+                () => console.log('connection failed'))
+            }
+          }
+        }, 
+        (err: HttpErrorResponse) => this.parsError(err));
+    }
+  }
+
+  checkPassword(): void{
+    let password = this.inputPasswordForm.controls['password']?.value;
+    if(password && TypeChecker.checkType<string>(password, 'length')){
+      this.chatGroup.Password = password;
+      this.dataService.postUserDatas<ChatGroup, DataShell>(this.chatGroup, 'check-password').subscribe(
+        () => {
+          this.chatingService
+          .connectToChat(this.chatGroup.Name, this.nick)
+          .subscribe(
+            () => {
+              console.log('connection success');
+              this.router.navigate(['/chat',this.chatGroup.Id])},
+            () => console.log('connection failed'))
+        }, 
+        (err: HttpErrorResponse) => this.parsError(err)
+      );
+    }
+  }
+  
+  parsError(error: HttpErrorResponse):void{
+    if(TypeChecker.checkType<DataShell>(error.error, 'result')){
+      console.warn(error.error.errors);
+    } else {
+      console.error('что-то пошло не так');
+    }
+  } 
+
+}
--- ChatProject/ChatProject/ClientApp/src/app/routing/app-routing.module.ts
+++ ChatProject/ChatProject/ClientApp/src/app/routing/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ChatComponent } from '../components/chat/chat.component';
 import { CreateComponent } from '../components/create/create.component';
 import { SearchComponent } from '../components/search/search.component';
 import { ErrorComponent } from '../components/error/error.component';
+import { ConnectComponent } from '../components/connect/connect.component';
 
 import {ConnectToChatGuard} from '../guards/connect-to-chat.guard'
 import { SharedModule } from '../modules/shared.module';
@@ -13,7 +14,10 @@ const routes: Routes = [
     {
         path: '',  children:[
             {
-                path: '', component: CreateComponent
+                path: '', component: ConnectComponent
+            },
+            {
+                path: 'connect', component: ConnectComponent
             },
             {
                 path: 'chat/:id', component: ChatComponent, canActivate: [ConnectToChatGuard]            
@@ -35,6 +39,7 @@ const routes: Routes = [
 ]
 @NgModule({
     declarations: [ 
+        ConnectComponent,
         ChatComponent,
         CreateComponent,
         SearchComponent,
